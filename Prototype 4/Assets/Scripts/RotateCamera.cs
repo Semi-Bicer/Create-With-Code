4e91a68d4f1ce69a5fed4d9e4d9bf6632ca6ea0c
Semi-Bicer <semih.bic.er@hotmail.com>
2026-03-14 19:20:46 +0300

    merge düzenlemesi

--- Prototype 4/Assets/Scripts/RotateCamera.cs
+++ Prototype 4/Assets/Scripts/RotateCamera.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
+<<<<<<< Updated upstream
     
     public float rotationSpeed;
+=======
+>>>>>>> Stashed changes
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +16,11 @@ public class RotateCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         float horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+=======
+        
+>>>>>>> Stashed changes
     }
 }
